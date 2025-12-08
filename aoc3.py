e757aee0c8c3b9c16c4f51db53447ce4c699943b
Damien Higgins <ninjaboydamo@icloud.com>
2025-12-08 23:26:20 +0000

    finished day 3 part 2 rewrote day 3 part 1 cleaned day 1, 2 and 3 up

--- aoc3.py
+++ aoc3.py
@@ -1,59 +1,46 @@
-total = 0
+######################################################day 1############################################################
+ans = 0
+######################################################part 1############################################################
 
-with open('inputs/input3', 'r') as file:
+
+with open("inputs/input3", "r") as file:
     for line in file:
-        # best = line.strip()
-        #
-        # total = 0
-        # count = -1
-        # difference = 0
-        # while len(best) > 12:
-        #     count += 1
-        #     highnum = -1
-        #     highpos = -1
-        #     for i in range(count,len(best)-1):
-        #         if count == 12:
-        #             best = best[:12]
-        #             break
-        #         elif not i < count:
-        #             num = best[i]
-        #             if num > str(highnum):
-        #                 highpos = i
-        #                 highnum = num
-        #
-        #             if i == len(best)-(12+count):
-        #                 best = best[len(best)-(12+count):]
-        #                 best = best[highpos:]
-        #                 break
-        #             if highpos > len(best)-12:
-        #                 best = best[:highpos]
-        #                 lowestnum = 0
-        #                 while len(best) > 12:
-        #                     for k in range(len(best)-11):
-        #                         if best[k] == lowestnum:
-        #                             temp = best
-        #                             best = temp[:k] + temp[k+1:]
-        #                             break
-        #                     lowestnum += 1
-        # print(best)
-        #
+        line = line.strip()
+        n = len(line)
 
+        best = -1
 
+        for i in range(n):
+            for j in range(i + 1, n):
+                pair = int(line[i] + line[j])
+                if pair > best:
+                    best = pair
+
+        ans += best
+
+print(ans)
+
+########################################################################################################################
+######################################################part 2############################################################
+with open("inputs/input3", "r") as file:
+    for line in file:
         line = line.strip()
         count = 0
-        while count <12:
+        best = ""
+        length = 0
+        while length < 12:
+
             highnum = 0
             highpos = 0
-            best = ""
-            print(line)
-            front =line[count:len(line)-(12-count)]
-            end = line[len(line)-(12-count):]
-            print(front)
-            print(end)
+            front = line[:len(line)-(11-length)]
             for j in range(len(front)):
-                if front[j] >= str(highnum):
-                    highnum = line[j]
+                if front[j] > str(highnum):
+                    highnum = front[j]
                     highpos = j
+
+            best = best + str(front[highpos])
             line = line[highpos+1:]
-            count += 1
+            length = len(best)
         print(best)
+        ans += int(best)
+    print(ans)
