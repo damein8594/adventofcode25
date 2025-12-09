57d5ca7a89bf2ed42526d3406a06c81138ce1d90
Damien Higgins <ninjaboydamo@icloud.com>
2025-12-09 23:00:43 +0000

    finished day 5 part 2

--- pyfiles/aoc5.py
+++ pyfiles/aoc5.py
@@ -1,21 +1,23 @@
-from shapely.speedups import available
+def handle():
+    ans = 0
+    ranges = []
+    numbers = []
+    with open("../inputs/input5", "r") as file:
+        for line in file:
+            if line == "\n":
+                continue
+            if "-" in line:
+                parts = line.split("-")
+                low = int(parts[0])
+                high = int(parts[1])
+                ranges.append((low, high))
+            else:
+                numbers.append(int(line))
 
-ans = 0
-ranges = []
-numbers = []
-with open("../inputs/input5", "r") as file:
-    for line in file:
-        if line == "\n":
-            continue
-        if "-" in line:
-            parts = line.split("-")
-            low = int(parts[0])
-            high = int(parts[1])
-            ranges.append((low, high))
-        else:
-            numbers.append(int(line))
+    return ans, numbers, ranges
 
 def p1():
+    ans,ranges,numbers = handle()
     for num in numbers:
         for low, high in ranges:
             if low <= num <= high:
@@ -24,33 +26,29 @@ def p1():
 
     print(ans)
 
-availableingrediants = []
-ranges.sort(key=lambda r: r[0])
-merged = []
-c_start, c_end = ranges[0][0], ranges[0][1]
 
-for i in range(1, len(ranges)):
-    start, end = ranges[i][0], ranges[i][1]
+def p2():
+    ans, numbers, ranges = handle()
+    availableingrediants = []
+    ranges.sort(key=lambda r: r[0])
+    merged = []
+    c_start, c_end = ranges[0][0], ranges[0][1]
 
-    if start <= c_end:
-        if end > c_end:
-            c_end = end
-    else:
+    for i in range(1, len(ranges)):
+        start, end = ranges[i][0], ranges[i][1]
 
-        merged.append((c_start, c_end))
-        c_start, c_end = start, end
+        if start <= c_end:
+            if end > c_end:
+                c_end = end
+        else:
+
+            merged.append((c_start, c_end))
+            c_start, c_end = start, end
 
 
-merged.append((c_start, c_end))
+    merged.append((c_start, c_end))
 
-for ranges in merged:
-    diff = ranges[1] - ranges[0]
-    print(diff)
-    ans+=diff
-
-# for start, end in merged:
-#     print(start, end)
-#     for i in range(start, end+1):
-#         print(i)
-#         ans += 1
-print(ans)
+    for ranges in merged:
+        diff = ranges[1] + 1 - ranges[0]
+        ans+=diff
+    print(ans)
