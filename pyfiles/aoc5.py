8ec378fc91877bdcbc6bb40ae1fd4636b024cd9a
Damien Higgins <ninjaboydamo@icloud.com>
2025-12-09 01:42:25 +0000

    part way through day 5 part 2

--- pyfiles/aoc5.py
+++ pyfiles/aoc5.py
@@ -1,3 +1,5 @@
+from shapely.speedups import available
+
 ans = 0
 ranges = []
 numbers = []
@@ -13,12 +15,42 @@ with open("../inputs/input5", "r") as file:
         else:
             numbers.append(int(line))
 
-for num in numbers:
-    for low, high in ranges:
-        if low <= num <= high:
-            ans += 1
-            break
+def p1():
+    for num in numbers:
+        for low, high in ranges:
+            if low <= num <= high:
+                ans += 1
+                break
 
+    print(ans)
+
+availableingrediants = []
+ranges.sort(key=lambda r: r[0])
+merged = []
+c_start, c_end = ranges[0][0], ranges[0][1]
+
+for i in range(1, len(ranges)):
+    start, end = ranges[i][0], ranges[i][1]
+
+    if start <= c_end:
+        if end > c_end:
+            c_end = end
+    else:
+
+        merged.append((c_start, c_end))
+        c_start, c_end = start, end
+
+
+merged.append((c_start, c_end))
+
+for ranges in merged:
+    diff = ranges[1] - ranges[0]
+    print(diff)
+    ans+=diff
+
+# for start, end in merged:
+#     print(start, end)
+#     for i in range(start, end+1):
+#         print(i)
+#         ans += 1
 print(ans)
-
-
